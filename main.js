a172ad04233b5fe10161e832a5c98ef6e85cf196
Martin Payne <martinpayne73@gmail.com>
2019-12-13 20:34:02 -0500

    make videos randomly sized and stay the same size as window

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let firstWindow = true
 const SCREEN_WIDTH = window.screen.availWidth
 const SCREEN_HEIGHT = window.screen.availHeight
-const WINDOW_WIDTH = 540
-const WINDOW_HEIGHT = 300
+let WINDOW_WIDTH = Math.max(Math.random() / 2, .2) * SCREEN_WIDTH;
+let WINDOW_HEIGHT = Math.max(Math.random() / 2, .2) * SCREEN_HEIGHT;
 const VELOCITY = 50
 const MARGIN = 10
 const TICK_LENGTH = 50
@@ -30,11 +30,27 @@ if (window.opener) {
   let vx = VELOCITY * (Math.random() - .5)
   let vy = VELOCITY * (Math.random() - .5)
 
-  window.setInterval(() => {
+  const videoElement = document.createElement('video')
+
+  let index = Math.floor(Math.random() * VIDEOS.length)
+
+  document.title = TITLES[index]
+
+  videoElement.src = VIDEOS[index]
+  videoElement.autoplay = true
+  videoElement.loop = true
+  videoElement.width = `${WINDOW_WIDTH}`
+  videoElement.height = `${WINDOW_HEIGHT}`
+
+  document.body.appendChild(videoElement)
+  
+    window.setInterval(() => {
     const x = window.screenX
     const y = window.screenY
     const width = window.outerWidth
+    videoElement.width = width
     const height = window.outerHeight
+    videoElement.height = height
 
     if (x < MARGIN) {
       vx = Math.abs(vx)
@@ -54,20 +70,6 @@ if (window.opener) {
 
     window.moveBy(vx, vy)
   }, TICK_LENGTH)
-
-  const videoElement = document.createElement('video')
-
-  let index = Math.floor(Math.random() * VIDEOS.length)
-
-  document.title = TITLES[index]
-
-  videoElement.src = VIDEOS[index]
-  videoElement.autoplay = true
-  videoElement.loop = true
-  videoElement.width = `${WINDOW_WIDTH}`
-  videoElement.height = `${WINDOW_HEIGHT}`
-
-  document.body.appendChild(videoElement)
 
   window.onunload = () => {
     if (!window.opener.closed) {
